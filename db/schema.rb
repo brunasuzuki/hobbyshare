2c05dc5364bf5681545e4da0bd1f9f1a9440d90e
bruna suzuki <brunasuzuki12@gmail.com>
2022-05-31 11:02:03 -0700

    started home banner

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_193419) do
+ActiveRecord::Schema.define(version: 2022_05_31_161901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "homepages", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "lectures", force: :cascade do |t|
     t.text "title"
