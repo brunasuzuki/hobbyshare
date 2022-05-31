a9b9733e7f4f9ab57729a4480d975f45265bb773
bruna suzuki <brunasuzuki12@gmail.com>
2022-05-31 14:05:36 -0700

    homepage WIP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home 
+  skip_before_action :authenticate_user!, only: :home
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   def home
+    @lectures = Lecture.all.first(4)
   end
 end
