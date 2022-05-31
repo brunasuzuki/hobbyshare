34cdf36a548803225684bc63d5ef4ee24258f4a6
bruna suzuki <brunasuzuki12@gmail.com>
2022-05-31 06:33:14 -0700

    validates lecture finished

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,4 +1,6 @@
 class Lecture < ApplicationRecord
   belongs_to :user
   has_many :orders, dependent: :destroy
+  validates :title, :description, :price, :youtube_url, presence: true
+  validates :description, numericality: { greater_than: 6 }
 end
