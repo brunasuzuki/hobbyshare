513146bb7d5bb5dc8289b4774d607e1cbb26a1e4
juliana lima <akemi.hiruma@gmail.com>
2022-06-01 12:13:10 -0300

    order controller done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,36 @@
 class OrdersController < ApplicationController
-  
+  before_action :set_lecture
+
+  def index
+    @orders = Order.all
+  end
+
+  def new
+    @lecture = Lecture.find(params[:lecture_id])
+  end
+
+  def create
+    @lecture = Lecture.find(params[:lecture_id])
+    @order = Order.new(lecture: @lecture, user: current_user)
+    if @order.save
+      redirect_to lecture_orders_path, alert: "Order created"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @order.destroy
+    redirect_to lectures_path(@lecture)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:new, :create)
+  end
+
+  def set_lecture
+    @lecture = Lecture.find(params[:lecture_id])
+  end
 end
